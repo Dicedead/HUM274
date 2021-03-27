c01731ea06eb36b20238d6ce39fbd5a28d06e3c7
Dicedead <dicedead@gmail.com>
2021-03-27 22:04:07 +0100

    translation int->note method done

--- mock_project/main.py
+++ mock_project/main.py
@@ -8,10 +8,6 @@ from custom_instruments import *
 string_res = run_complex_for(4)
 # print(string_res)
 sequence = sequence_from_string_complex(string_res)
-# rhythm = rhythm_from_sequence(sequence, instrument.Piano(), '4/4')
-# rhythm.write('midi', 'midi/complex_rhythm_treatment.mid')
-# print('Done!')
-
 
 some_length = len(sequence)
 parts = combine_voices(some_length, sequence,
@@ -19,3 +15,4 @@ parts = combine_voices(some_length, sequence,
                        inst=[instrument.Piano()])
 parts.write('midi', 'midi/test_newrules.mid')
 print("Done!")
+
